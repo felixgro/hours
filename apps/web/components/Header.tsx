c27a52143ac84fb361446ce9354be4d351d7f1c0
felixgro <me@felixgrohs.dev>
2022-01-31 18:14:11 +0100

    add working day button

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -13,7 +13,9 @@ export const Header: React.FC<HeaderProps> = () => {
 		<header>
 			<div>
 				<VerticalLine />
-				<img src="/static/icons/logo.svg" height="32" width="32" alt="Logo" />
+				<a href="/" style={{ height: '32px' }} aria-label="Return to landing page">
+					<img src="/static/icons/logo.svg" height="32" width="32" alt="Logo" />
+				</a>
 				<VerticalLine />
 			</div>
 			<nav>
@@ -21,7 +23,13 @@ export const Header: React.FC<HeaderProps> = () => {
 				{session.status === 'unauthenticated' ? (
 					<Button label="Login / Register" onClick={signIn} />
 				) : (
-					<ProfileMenu {...session.data?.user} />
+					<>
+						<Button label="Add Working day" onClick={() => console.log('clicked')}>
+							Hello Threr
+						</Button>
+						<VerticalLine />
+						<ProfileMenu {...session.data?.user} />
+					</>
 				)}
 				<VerticalLine />
 			</nav>
