7ce443f0309954f0389b20e93d11e88933f777f0
felixgro <me@felixgrohs.dev>
2022-01-31 15:02:53 +0100

    add google sign in

--- apps/web/pages/api/auth/[...nextauth].ts
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
-import NextAuth from "next-auth";
+import NextAuth, { Awaitable, User } from "next-auth";
+import { JWT } from 'next-auth/jwt';
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
@@ -6,6 +7,30 @@ export default NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            },
         }),
     ],
+    jwt: {
+        secret: process.env.JWT_SECRET,
+    },
+    callbacks: {
+        jwt: async ({ token, account }) => {
+            if (account?.access_token) {
+                token.access_token = account.access_token;
+            }
+            return token;
+        },
+        redirect: async ({ url, baseUrl }) => {
+            if (url === '/profile') {
+                return Promise.resolve('/');
+            }
+            return Promise.resolve('/');
+        }
+    }
 });
